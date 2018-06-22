ab6f2c20f8cd367fd5d206d1c62ea17dfc351aeb
kongdexing <>
2018-06-22 11:14:15 +0800

    impl clear cache

--- app/src/main/java/com/example/ysl/mywps/ui/fragment/MineFragment.java
+++ app/src/main/java/com/example/ysl/mywps/ui/fragment/MineFragment.java
@@ -1,7 +1,6 @@
 package com.example.ysl.mywps.ui.fragment;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +9,15 @@ import android.widget.TextView;
 
 import com.example.ysl.mywps.R;
 import com.example.ysl.mywps.ui.activity.LoginActivity;
-import com.example.ysl.mywps.utils.NoDoubleClickListener;
+import com.example.ysl.mywps.ui.activity.MainActivity;
+import com.example.ysl.mywps.utils.DataCleanManager;
 import com.example.ysl.mywps.utils.SharedPreferenceUtils;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/12/23 0023.
@@ -26,8 +29,8 @@ public class MineFragment extends BaseFragment {
     RelativeLayout rlLoginOut;
     @BindView(R.id.mine_tv_name)
     TextView mineTvName;
-
-    private MyclickListener click = new MyclickListener();
+    @BindView(R.id.txtCache)
+    TextView txtCache;
 
     @Override
     public void initData() {
@@ -45,10 +48,21 @@ public class MineFragment extends BaseFragment {
 
     @Override
     public void afterView(View view) {
-
-        rlLoginOut.setOnClickListener(click);
-        String name = SharedPreferenceUtils.loginValue(getActivity(),"name");
+        String name = SharedPreferenceUtils.loginValue(getActivity(), "name");
         mineTvName.setText(name);
+
+        calculateCache();
+
+    }
+
+    private void calculateCache() {
+        //获得应用内部缓存(/data/data/com.example.androidclearcache/cache)
+        File file = new File(this.getContext().getCacheDir().getPath());
+        try {
+            txtCache.setText(DataCleanManager.getCacheSize(file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -56,23 +70,29 @@ public class MineFragment extends BaseFragment {
 
     }
 
-    private class MyclickListener extends NoDoubleClickListener {
-        @Override
-        public void click(View v) {
-
-            switch (v.getId()) {
-
-                case R.id.mine_rl_loginout:
-
-                    SharedPreferenceUtils.loginSave(getActivity(), "token", "");
-
-                    Intent intent = new Intent(getActivity(), LoginActivity.class);
-                    startActivity(intent);
-                    getActivity().finish();
-                    break;
-
-            }
+    @OnClick({R.id.rlClearCache, R.id.mine_rl_loginout})
+    void viewClick(View view) {
+        switch (view.getId()) {
+            case R.id.mine_rl_loginout:
+                SharedPreferenceUtils.loginSave(getActivity(), "token", "");
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+                break;
+            case R.id.rlClearCache:
+                ((MainActivity) this.getContext()).showProgress("清理中...");
+                try {
+//                    DataCleanManager.cleanInternalCache(getContext());
+                    DataCleanManager.deleteFolderFile(this.getContext().getCacheDir().getPath(), false);
+                    calculateCache();
+                    ((MainActivity) this.getContext()).hideProgress();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ((MainActivity) this.getContext()).hideProgress();
+                }
+                break;
 
         }
     }
+
 }
